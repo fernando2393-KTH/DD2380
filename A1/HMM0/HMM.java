a120e76d2fb2c7ddd7edd2246b116a6437a75ffb
fernando2393 <garciasanzfernando2393@gmail.com>
2019-09-04 17:34:15 +0200

    HMM0 and HMM1 conventions modified

--- A1/HMM0/HMM.java
+++ A1/HMM0/HMM.java
@@ -8,15 +8,14 @@ public class HMM {
     static double[][] A;
     static double[][] B;
     static double[][] pi;
-    static matrixOps mat_ops = new matrixOps();
 
     // Reads values from console and populates A, B, pi
     public static void read_hmm() {
         BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
         try {
-            A = mat_ops.parse_matrix(reader.readLine());
-            B = mat_ops.parse_matrix(reader.readLine());
-            pi = mat_ops.parse_matrix(reader.readLine());   
+            A = matrixOps.parse_matrix(reader.readLine());
+            B = matrixOps.parse_matrix(reader.readLine());
+            pi = matrixOps.parse_matrix(reader.readLine());   
         } catch (Exception e) {
             System.err.println("Could not read from System.in");
         }
@@ -34,9 +33,9 @@ public class HMM {
 
     // Returns next emission probability
     public static void next_emission() {
-        double[][] state_prob = mat_ops.multiply(pi, A);
-        double[][] emission_prob = mat_ops.multiply(state_prob, B);
-        mat_ops.print_matrix(emission_prob);
+        double[][] state_prob = matrixOps.multiply(pi, A);
+        double[][] emission_prob = matrixOps.multiply(state_prob, B);
+        matrixOps.print_matrix(emission_prob);
     }
 
     // PRIVATE
