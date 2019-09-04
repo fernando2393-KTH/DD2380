87776657a7f6d02b1d084d9ea88df0dbd8e307c9
OleguerCanal <oleguer.canal@hotmail.com>
2019-09-04 17:34:20 +0200

    Modified readers

--- A1/HMM1/HMM.java
+++ A1/HMM1/HMM.java
@@ -1,17 +1,25 @@
 // Class to hold HMM
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import matrixOps;
 
 public class HMM {
     static double[][] A;
     static double[][] B;
     static double[][] pi;
+    static matrixOps mat_ops = new matrixOps();
 
     // Reads values from console and populates A, B, pi
     public static void read_hmm() {
-        A = matrixOps.read_matrix();
-        B = matrixOps.read_matrix();
-        pi = matrixOps.read_matrix();   
+        BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
+        try {
+            A = mat_ops.parse_matrix(reader.readLine());
+            B = mat_ops.parse_matrix(reader.readLine());
+            pi = mat_ops.parse_matrix(reader.readLine());   
+        } catch (Exception e) {
+            System.err.println("Could not read from System.in");
+        }
     }
 
     // Reads values from console and populates A, B, pi
@@ -26,9 +34,9 @@ public class HMM {
 
     // Returns next emission probability
     public static void next_emission() {
-        double[][] state_prob = matrixOps.multiply(pi, A);
-        double[][] emission_prob = matrixOps.multiply(state_prob, B);
-        matrixOps.print_matrix(emission_prob);
+        double[][] state_prob = mat_ops.multiply(pi, A);
+        double[][] emission_prob = mat_ops.multiply(state_prob, B);
+        mat_ops.print_matrix(emission_prob);
     }
 
     // PRIVATE
