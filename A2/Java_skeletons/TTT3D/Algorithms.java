4f9f07e7aadf43804d62aeecfcf7c45de4f9228e
fernando2393 <garciasanzfernando2393@gmail.com>
2019-09-23 16:37:25 +0200

    Changed for multilayer algorithm

--- A2/Java_skeletons/TTT3D/Algorithms.java
+++ A2/Java_skeletons/TTT3D/Algorithms.java
@@ -52,9 +52,9 @@ public class Algorithms {
         gameState.findPossibleMoves(nextStates);
 
         if (gameState.isXWin())
-            return new Pair(0, Integer.MAX_VALUE);
+            return new Pair<Integer, Integer>(0, Integer.MAX_VALUE);
         if (gameState.isOWin())
-            return new Pair(0, Integer.MIN_VALUE);
+            return new Pair<Integer, Integer>(0, Integer.MIN_VALUE);
 
         // If terminal state
         if (depth == 0 || gameState.isEOG())
@@ -99,7 +99,7 @@ public class Algorithms {
      * @param player    the player who is playing in the turn
      * @return the total gains of moving to gameState
      */
-    public int evaluation(GameState gameState, int player) {
+    public int evaluationLayer(int [][] mat, int player) {
         int result = 0;
 
         /*********** Rows checking ***********/
@@ -107,9 +107,9 @@ public class Algorithms {
             int numberX = 0; // Number of X in the line
             int numberO = 0; // Number of O in the line
             for (int j = 0; j < GameState.BOARD_SIZE; j++) {
-                if (gameState.at(i, j) == Constants.CELL_X) {
+                if (mat[i][j] == Constants.CELL_X) {
                     numberX++;
-                } else if (gameState.at(i, j) == Constants.CELL_O) {
+                } else if (mat[i][j] == Constants.CELL_O) {
                     numberO++;
                 }
             }
@@ -121,9 +121,9 @@ public class Algorithms {
             int numberX = 0; // Number of X in the column
             int numberO = 0; // Number of O in the column
             for (int j = 0; j < GameState.BOARD_SIZE; j++) {
-                if (gameState.at(j, i) == Constants.CELL_X) {
+                if (mat[j][i] == Constants.CELL_X) {
                     numberX++;
-                } else if (gameState.at(j, i) == Constants.CELL_O) {
+                } else if (mat[j][i] == Constants.CELL_O) {
                     numberO++;
                 }
             }
@@ -135,9 +135,9 @@ public class Algorithms {
         int numberX = 0; // Number of X in the first diagonal
         int numberO = 0; // Number of O in the first diagonal
         for (int i = 0; i < GameState.BOARD_SIZE; i++) {
-            if (gameState.at(i, i) == Constants.CELL_X) {
+            if (mat[i][i] == Constants.CELL_X) {
                 numberX++;
-            } else if (gameState.at(i, i) == Constants.CELL_O) {
+            } else if (mat[i][i] == Constants.CELL_O) {
                 numberO++;
             }
         }
@@ -147,9 +147,9 @@ public class Algorithms {
         numberX = 0; // Number of X in the second diagonal
         numberO = 0; // Number of O in the second diagonal
         for (int i = 0; i < GameState.BOARD_SIZE; i++) {
-            if (gameState.at(i, (GameState.BOARD_SIZE - 1) - i) == Constants.CELL_X) {
+            if (mat[i][(GameState.BOARD_SIZE - 1) - i] == Constants.CELL_X) {
                 numberX++;
-            } else if (gameState.at(i, (GameState.BOARD_SIZE - 1) - i) == Constants.CELL_O) {
+            } else if (mat[i][(GameState.BOARD_SIZE - 1) - i] == Constants.CELL_O) {
                 numberO++;
             }
         }
